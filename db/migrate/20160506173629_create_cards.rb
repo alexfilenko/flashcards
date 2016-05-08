62f2e1bbda81ca0ea4e4ec0c7570f9f46871e9d6
alexfilenko <filenkoav@gmail.com>
2016-05-08 16:40:56 +0300

    attr_accessible

--- db/migrate/20160506173629_create_cards.rb
+++ db/migrate/20160506173629_create_cards.rb
@@ -1,7 +1,8 @@
 class CreateCards < ActiveRecord::Migration
   def change
+  	attr_accessible :original_text, :translated_text, :review_date
     create_table :cards do |t|
-      #t.string][ :[field[
+      t.string][ :[field[
       t.string 	:original_text
       t.string 	:translated_text
       t.string 	:review_date
