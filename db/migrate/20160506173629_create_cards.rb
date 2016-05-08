6531149e56f05af0450cf1e6b8ff8a2fd51db26b
alexfilenko <filenkoav@gmail.com>
2016-05-08 16:18:11 +0300

    Миграция для для модели Card

--- db/migrate/20160506173629_create_cards.rb
+++ db/migrate/20160506173629_create_cards.rb
@@ -2,7 +2,9 @@ class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
       t.string][ :[field[
-
+      t.string 	:original_text
+      t.string 	:translated_text
+      t.string 	:review_date
       t.timestamps null: false
     end
   end
