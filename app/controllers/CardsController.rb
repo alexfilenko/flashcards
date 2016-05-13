953b63d5dca5400dfe8acc83788ee40c27537a8d
alexfilenko <filenkoav@gmail.com>
2016-05-13 20:05:02 +0300

    13/05

--- app/controllers/CardsController.rb
+++ app/controllers/CardsController.rb
@@ -3,6 +3,24 @@ class CardsController < ApplicationController
    @cards = Card.all
   end
 
+  def show
+  	@cards = Cards.find(params[:id])
+
   def create
+  end
+
+  def create
+  @cards = Cards.new(carts_params)
+ 
+  if @cards.save
+  redirect_to @cards
+else
+	render 'new'
+end
+end
+ 
+private
+  def cards_params
+    params.require(:cards).permit(:title, :text)
   end
 end
