fc941f14ba2fa3114ccf2dc9ca20b7358b4451cb
alexfilenko <filenkoav@gmail.com>
2016-05-14 17:31:07 +0300

    uniqueness

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,4 @@
 class Card < ActiveRecord::Base
 	validates :original_text, :translated_text presence: true,
-                    
+    validates :original_text, :translated_text uniqueness: true               
 end
