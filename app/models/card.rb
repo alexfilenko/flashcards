824ed3a20f43213f96db0f606d053d50ec09818d
alexfilenko <filenkoav@gmail.com>
2016-05-14 17:50:24 +0300

    data

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,17 @@
 class Card < ActiveRecord::Base
-	validates :original_text, :translated_text presence: true,
-    validates :original_text, :translated_text uniqueness: true               
+	validates :original_text, :translated_text, presence: true
+    validates :original_text, :translated_text, uniqueness: true               
+	validate :check_original_and_translated_text
+    before_create :set_time_to_now
+
+  def check_original_and_translated_text
+    if original_text == translated_text || original_text == translated_text.capitalize
+      errors.add(:original_text, "can't be similar as translated text")
+    end
+  end
+
+  def set_time_to_now
+    self.review_date = Time.now + 3.days
+  end
 end
+
